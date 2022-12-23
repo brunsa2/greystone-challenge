cf93db338289d7947704b9d15c1f14c6f49e6361
Jeff Stubler <brunsa2@gmail.com>
2022-12-22 23:04:28 -0600

    Add support for showing authorized user loans when getting loan information and also showing shared loans with shared users

--- repositories/authorized_user_repository.py
+++ repositories/authorized_user_repository.py
@@ -6,6 +6,8 @@ class AuthorizedUserRepository:
     """Memory-based authorized user repository"""
     def __init__(self):
         self.authorized_users = {}
+        self.authorized_users_by_loan_id = {}
+        self.authorized_users_by_user_id = {}
         self.next_id = 1
 
     def create(self, authorized_user: AuthorizedUser):
@@ -19,6 +21,8 @@ class AuthorizedUserRepository:
         self.next_id += 1
         authorized_user.authorized_user_id = next_id
         self.authorized_users[next_id] = authorized_user
+        self.authorized_users_by_loan_id.setdefault(authorized_user.loan_id, []).append(authorized_user)
+        self.authorized_users_by_user_id.setdefault(authorized_user.user_id, []).append(authorized_user)
 
     def read(self, authorized_user_id: int):
         """Read authorized user from the repository.
@@ -26,3 +30,18 @@ class AuthorizedUserRepository:
         @:returns: Authorized user object
         """
         return self.authorized_users.get(authorized_user_id)
+
+    def read_authorized_user_ids(self, loan_id: int):
+        """Read authorized user IDs from the repository for a given loan.
+        @:param loan: ID of loan to read authorized user IDs from
+        @:returns: Array of authorized user IDs
+        """
+        return [authorized_user.user_id
+                for authorized_user in self.authorized_users_by_loan_id.setdefault(loan_id, [])]
+
+    def read_loan_ids_by_user_id(self, user_id: int):
+        """Read loan IDs from the repository for a authorized user ID.
+        @:param loan: ID of user to read authorized user IDs from
+        @:returns: Array of loan IDs
+        """
+        return [authorized_user.loan_id for authorized_user in self.authorized_users_by_user_id.setdefault(user_id, [])]
--- services/get_user_loans_service.py
+++ services/get_user_loans_service.py
@@ -5,13 +5,24 @@ from request_exception import RequestException
 class GetUserLoansService:
     """Service for getting all loans from a user"""
     @staticmethod
-    def get_user_loans(loan_repository, user_repository, user_id: int):
+    def get_user_loans(loan_repository, user_repository, authorized_user_repository, user_id: int):
         """Get all loans from a user
         @:param loan_repository: Repository to read loans from
+        @:param user_repository: Repository to read users from
+        @:param authorized_user_repository: Repository to read authorized users from
         @:param user_id: User ID to get loans from
         @:returns: list of loans belonging to user
         @:raises: RequestException if request is invalid
         """
         if not user_repository.exists(user_id):
             raise RequestException("missing user {}".format(user_id))
-        return loan_repository.read_loans_for_user_id(user_id)
+        loans = loan_repository.read_loans_for_user_id(user_id)
+
+        authorized_loan_ids = authorized_user_repository.read_loan_ids_by_user_id(user_id)
+        for authorized_loan_id in authorized_loan_ids:
+            loans.append(loan_repository.read(authorized_loan_id))
+
+        for loan in loans:
+            loan.authorized_user_ids = authorized_user_repository.read_authorized_user_ids(loan.loan_id)
+
+        return loans
